ae1d63f4fd09d35970d88c40390b1f666234544e
user.name = 'Yale Work <user.email = 'yale.work@gmail.com>
2017-06-11 15:16:23 -0700

    Sync react with djangogit aagit aa

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve('./staticfiles/review/index.entry.js'),
+  output: {
+    path: path.resolve(__dirname, 'staticfiles/review'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+}
