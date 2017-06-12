40a8ecc59c992db56f6296c19e09fba2288c9bbc
user.name = 'Yale Work <user.email = 'yale.work@gmail.com>
2017-06-12 02:01:22 -0700

    Improve error messages slightly

--- utils/word_utils.py
+++ utils/word_utils.py
@@ -1,11 +1,13 @@
 def get_words(language, max_words=None):
     words = []
-    with open('./bin/resources/%s/%s_words.txt' % (language, language), 'r') as f:
+    path = './bin/resources/%s/%s_words.txt' % (language, language)
+    with open(path, 'r') as f:
         for idx, line in enumerate(f.readlines()):
             if max_words is not None and idx == max_words:
                 break
             if line.count('    ') != 1 or line.count(': ') != 1:
-                raise Exception('Invalid format on line %s' % (idx + 1))
+                raise Exception(
+                    'Invalid format on line %s in file %s' % ((idx + 1), path))
             line = line.strip()
             foreign, pron_eng = line.split('    ')
             pron, eng = pron_eng.split(': ')
